42233686b461d7e8104ac984901e93481cd51093
Felipe <felipe.oliveira.tk@gmail.com>
2018-02-06 15:18:51 -0200

    resultados d10

--- v9/SalaCozinha/simulateAndExit.m
+++ v9/SalaCozinha/simulateAndExit.m
@@ -35,7 +35,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Loop for simulation time steps.
 simulate = 1;
-tcp = tcpip('localhost', 50003);
+tcp = tcpip('localhost', 50013);
 fopen(tcp);
 
 while (simulate)
