80a0b85e96c1b19a75fa745f340d5d33d3842d4c
Felipe <felipe.oliveira.tk@gmail.com>
2018-02-07 10:57:05 -0200

    estruturas finais definidas

--- v8/Python/waiter.py
+++
@@ -1,4 +0,0 @@
-import time
-import datetime
-print ("WAITER | " + str(datetime.datetime.now()))
-time.sleep(1) #substituir para 20
---
+++ v9/QuartoCasal/Python/waiter.py
@@ -0,0 +1,4 @@
+import time
+import datetime
+print ("WAITER | " + str(datetime.datetime.now()))
+time.sleep(1) #substituir para 20
